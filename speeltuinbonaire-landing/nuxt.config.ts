b6784b06373dcb4d369502c714262deb9519fe34
Solidverse <t.amatk@protonmail.com>
2025-01-29 14:09:18 +0100

    add nuxt-icon

--- speeltuinbonaire-landing/nuxt.config.ts
+++ speeltuinbonaire-landing/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/eslint", "@nuxt/image"],
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/icon"],
   vite: {
     plugins: [tailwindcss()],
   },
